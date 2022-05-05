9a3e407458170c4719d85c675656e8045100e4a2
Gabriel Bustos <gabrielbustos0905@gmail.com>
2022-05-05 10:35:52 -0300

    Second commit

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,12 +2,27 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  // num = 1011
+  //       0123
 
+  var suma = 0
+  for(let i=0; i<num.length; i++){
+    let posicion = num.length - 1 - i
+    suma += num[i] * Math.pow(2, posicion)
+  }
+  return suma
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  // num = 250
 
+  var concat = []
+  while(num > 0){
+    concat.unshift(Math.floor(num%2))
+    num = Math.floor(num/2)
+  }
+  return concat.join("")
 }
 
 
